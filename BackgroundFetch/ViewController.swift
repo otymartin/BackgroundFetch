465c863702c66964ebfa4ff5625566cd3e03dd72
Martin Otyeka <otyeka.martin@gmail.com>
2018-02-21 20:49:55 -0500

    Local push works

--- BackgroundFetch/ViewController.swift
+++ BackgroundFetch/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in }
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.tap))
+        self.view.addGestureRecognizer(tap)
+    }
+    
+    @objc func tap() {
+        let content = UNMutableNotificationContent()
+        content.title = "Views"
+        content.badge = 1
+        content.body = "You have been viewed 5 times by people around you!"
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "fire", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request) { (error) in
+            print(error?.localizedDescription)
+        }
     }
 
     override func didReceiveMemoryWarning() {
